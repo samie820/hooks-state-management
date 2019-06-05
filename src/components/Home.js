8a8ff2d22ce1b04a61fab49513a39c4b968dfec7
samie820 <samuelomole70@gmail.com>
2019-06-05 23:51:50 +0100

    Created the Home component and added the effect for getting songs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { AuthContext } from "../App";
 
 export const Home = () => {
-  return (
-    <div>
-    </div>
-  );
+  const { state } = React.useContext(AuthContext);
+  React.useEffect(() => {
+    fetch("http://localhost:8000/api/songs", {
+      headers: {
+        Authorization: `Bearer ${state.token}`
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res;
+        }
+      })
+      .then(resJson => {
+        console.log(resJson);
+      });
+  }, []);
+
+  return <div />;
 };
 
 export default Home;
